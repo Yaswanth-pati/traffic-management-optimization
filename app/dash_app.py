ad5ea370d13e7270ef964729ee4d52461ed8e9f4
yashwanth1289 <yaswanth.pati426@gmail.com>
2024-05-05 03:53:02 -0400

    removed unwanted files

--- app/dash_app.py
+++ app/dash_app.py
@@ -3,151 +3,177 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
-# Load your dataset
-df = pd.read_csv('traffic-volume-survey.csv')
+# Loading the dataset
+df = pd.read_csv("traffic-volume-survey.csv")
 
 app = Dash(__name__, suppress_callback_exceptions=True)
 
+
 def create_volume_histogram(data):
-    fig = px.histogram(data, x='volume_24h', color='road_name',
-                       title='Distribution of 24-Hour Traffic Volume',
-                       labels={'volume_24h': '24-Hour Traffic Volume'},
-                       barmode='group', template='plotly_white')
+    fig = px.histogram(
+        data,
+        x="volume_24h",
+        color="road_name",
+        title="Distribution of 24-Hour Traffic Volume",
+        labels={"volume_24h": "24-Hour Traffic Volume"},
+        barmode="group",
+        template="plotly_white",
+    )
     fig.update_traces(opacity=0.75)
     fig.update_layout(
         title_font_size=22,
-        title_font_family='Verdana',
-        legend_title_text='Road',
+        title_font_family="Verdana",
+        legend_title_text="Road",
         xaxis_title="24-Hour Volume",
         yaxis_title="Count",
-        plot_bgcolor='rgba(0,0,0,0)',
-        paper_bgcolor='rgba(255,255,255,1)'
+        plot_bgcolor="rgba(0,0,0,0)",
+        paper_bgcolor="rgba(255,255,255,1)",
     )
     return fig
-
 
 
 # Function to create a scatter plot of Volume vs. Speed
 def create_volume_speed_scatter(data):
-    fig = px.scatter(data, x='volume_24h', y='range', color='road_name',
-                     title='Traffic Volume vs. Speed',
-                     labels={'volume_24h': '24-Hour Volume', 'range': 'Speed (km/h)'},
-                     template='plotly_white')
-    fig.update_traces(marker=dict(size=10, opacity=0.8, line=dict(width=2, color='DarkSlateGrey')))
+    fig = px.scatter(
+        data,
+        x="volume_24h",
+        y="range",
+        color="road_name",
+        title="Traffic Volume vs. Speed",
+        labels={"volume_24h": "24-Hour Volume", "range": "Speed (km/h)"},
+        template="plotly_white",
+    )
+    fig.update_traces(
+        marker=dict(size=10, opacity=0.8, line=dict(width=2, color="DarkSlateGrey"))
+    )
     fig.update_layout(
         title_font_size=22,
-        legend_title_text='Road',
+        legend_title_text="Road",
         xaxis_title="24-Hour Volume",
-        yaxis_title="Speed (km/h)"
+        yaxis_title="Speed (km/h)",
     )
     return fig
 
+
 # Function to create a scatter plot of Volume vs. Speed
 def create_volume_speed_year_scatter(data):
-    fig = px.scatter(data, x='volume_24h', y='installed_year', color='road_name',
-                     title='Traffic Volume vs. Speed',
-                     labels={'volume_24h': '24-Hour Volume', 'range': 'Speed (km/h)'},
-                     template='plotly_white')
-    fig.update_traces(marker=dict(size=10, opacity=0.8, line=dict(width=2, color='DarkSlateGrey')))
+    fig = px.scatter(
+        data,
+        x="volume_24h",
+        y="installed_year",
+        color="road_name",
+        title="Traffic Volume vs. Speed",
+        labels={"volume_24h": "24-Hour Volume", "range": "Speed (km/h)"},
+        template="plotly_white",
+    )
+    fig.update_traces(
+        marker=dict(size=10, opacity=0.8, line=dict(width=2, color="DarkSlateGrey"))
+    )
     fig.update_layout(
         title_font_size=22,
-        legend_title_text='Road',
+        legend_title_text="Road",
         xaxis_title="24-Hour Volume",
-        yaxis_title="Speed (km/h)"
+        yaxis_title="Speed (km/h)",
     )
     return fig
 
 
 # Function to create a bar chart of Peak Traffic Times
 def create_peak_times_bar(data):
-    fig = px.bar(data, x='road_name', y='peakvol', color='road_name',
-                 title='Peak Traffic Volumes by Road',
-                 labels={'peakvol': 'Peak Volume', 'road_name': 'Road Name'},
-                 template='plotly_white')
+    fig = px.bar(
+        data,
+        x="road_name",
+        y="peakvol",
+        color="road_name",
+        title="Peak Traffic Volumes by Road",
+        labels={"peakvol": "Peak Volume", "road_name": "Road Name"},
+        template="plotly_white",
+    )
     fig.update_layout(
         title_font_size=22,
-        legend_title_text='Road',
+        legend_title_text="Road",
         xaxis_title="Road Name",
         yaxis_title="Peak Volume",
-        plot_bgcolor='rgba(0,0,0,0)',
-        paper_bgcolor='rgba(255,255,255,1)'
+        plot_bgcolor="rgba(0,0,0,0)",
+        paper_bgcolor="rgba(255,255,255,1)",
     )
-    fig.update_traces(marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6)
+    fig.update_traces(
+        marker_line_color="rgb(8,48,107)", marker_line_width=1.5, opacity=0.6
+    )
     return fig
 
 
-app.layout = html.Div([
-    html.H1("Traffic Data Visualization", style={'textAlign': 'center', 'color': '#007BFF'}),
-    html.Div([  # This div will contain the dropdowns and use flexbox to align them side by side
-        dcc.Dropdown(
-            id='road-dropdown',
-            options=[{'label': road, 'value': road} for road in df['road_name'].unique()],
-            value=[],
-            multi=True,
-            clearable=False,
-            placeholder="Select a road",
-            style={'padding': '3px', 'minWidth': '300px'}
+app.layout = html.Div(
+    [
+        html.H1(
+            "Traffic Data Visualization",
+            style={"textAlign": "center", "color": "#007BFF"},
         ),
-        dcc.Dropdown(
-            id='year-dropdown',
-            options=[{'label': year, 'value': year} for year in sorted(df['installed_year'].unique())],
-            value=[],
-            multi=True,
-            clearable=True,
-            placeholder="Select installation year",
-            style={'padding': '3px','minWidth': '300px'}
+        html.Div(
+            [
+                dcc.Dropdown(
+                    id="road-dropdown",
+                    options=[
+                        {"label": road, "value": road}
+                        for road in df["road_name"].unique()
+                    ],
+                    value=[],
+                    multi=True,
+                    clearable=False,
+                    placeholder="Select a road",
+                    style={"padding": "3px", "minWidth": "300px"},
+                ),
+            ],
+            style={"display": "flex", "justifyContent": "space-between"},
         ),
-    ], style={'display': 'flex', 'justifyContent': 'space-between'}), 
-    dcc.Graph(id='traffic-volume-graph'),
-    dcc.Graph(id='volume-speed-scatter-graph'),
-    dcc.Graph(id='peak-times-bar-graph'),
-], style={'padding': '20px', 'backgroundColor': '#FAFAFA'})
-
-
-# Callback to update the traffic volume graph
-@app.callback(
-    Output('traffic-volume-graph', 'figure'),
-    Input('road-dropdown', 'value')
+        dcc.Graph(id="traffic-volume-graph"),
+        dcc.Graph(id="volume-speed-scatter-graph"),
+        dcc.Graph(id="peak-times-bar-graph"),
+    ],
+    style={"padding": "20px", "backgroundColor": "#FAFAFA"},
 )
+
+
+# Callback for traffic volume graph
+@app.callback(Output("traffic-volume-graph", "figure"), Input("road-dropdown", "value"))
 def update_traffic_graph(selected_roads):
     try:
         if not selected_roads:
             return {}
-        filtered_df = df[df['road_name'].isin(selected_roads)]
+        filtered_df = df[df["road_name"].isin(selected_roads)]
         return create_volume_histogram(filtered_df)
     except Exception as e:
         print("Error updating traffic graph:", e)
         return {}
 
-# Callback to update the volume-speed scatter graph
+
+# Callback for volume-speed scatter graph
 @app.callback(
-    Output('volume-speed-scatter-graph', 'figure'),
-    Input('road-dropdown', 'value')
+    Output("volume-speed-scatter-graph", "figure"), Input("road-dropdown", "value")
 )
 def update_volume_speed_scatter(selected_roads):
     try:
         if not selected_roads:
             return {}
-        filtered_df = df[df['road_name'].isin(selected_roads)]
+        filtered_df = df[df["road_name"].isin(selected_roads)]
         return create_volume_speed_scatter(filtered_df)
     except Exception as e:
         print("Error updating volume-speed scatter:", e)
         return {}
 
-# Callback to update the peak times bar graph
-@app.callback(
-    Output('peak-times-bar-graph', 'figure'),
-    Input('road-dropdown', 'value')
-)
+
+# Callback for peak times bar graph
+@app.callback(Output("peak-times-bar-graph", "figure"), Input("road-dropdown", "value"))
 def update_peak_times_bar(selected_roads):
     try:
         if not selected_roads:
             return {}
-        filtered_df = df[df['road_name'].isin(selected_roads)]
+        filtered_df = df[df["road_name"].isin(selected_roads)]
         return create_peak_times_bar(filtered_df)
     except Exception as e:
         print("Error updating peak times bar graph:", e)
         return {}
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     app.run_server(debug=False)
--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-#Background Styling
+
+# Background
 def add_bg_from_local():
     st.markdown(
         f"""
@@ -14,75 +15,80 @@ def add_bg_from_local():
         }}
         </style>
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
 
 
 add_bg_from_local()
 
 # App Header
-st.title("Simple Traffic Risk Prediction App")
-st.write("Enter the parameters and predict the traffic risk level.")
+st.title(" Traffic Prediction App")
+st.write("Enter the parameters and predict the traffic level")
 
-#Loading the  Trained Model
-lr_model = joblib.load('optimized_model.pkl')
-logistic_model = joblib.load('log_model.pkl')
-decision_model = joblib.load('dec_model.pkl')
+# Loading the  Trained Model
+lr_model = joblib.load("optimized_model.pkl")
+logistic_model = joblib.load("log_model.pkl")
+decision_model = joblib.load("dec_model.pkl")
 
-# Function to Collect User Inputs and Predict
-def user_input_features():
-    col1, col2, col3 = st.columns(3)  # Create three columns
+
+# Function to enter the input and predict
+def user_input():
+    col1, col2, col3 = st.columns(3)
     with col1:
-        pc85th_kmh = st.number_input('85th Percentile Speed', min_value=0)
-        volume_12h = st.number_input('12 Hour Volume', min_value=0)
+        pc85th_kmh = st.number_input("85th Percentile Speed", min_value=0)
+        volume_12h = st.number_input("12 Hour Volume", min_value=0)
     with col2:
-        volume_24h = st.number_input('24 Hour Volume', min_value=0)
-        comm_vhcl = st.number_input('Commercial Vehicle Volume', min_value=0.0)
+        volume_24h = st.number_input("24 Hour Volume", min_value=0)
+        comm_vhcl = st.number_input("Commercial Vehicle Volume", min_value=0.0)
     with col3:
-        peakvol = st.number_input('Peak Volume', min_value=0.0)
-    
-    return pd.DataFrame({
-        'pc85th_kmh': [pc85th_kmh],
-        'volume_12h': [volume_12h],
-        'volume_24h': [volume_24h],
-        'comm_vhcl': [comm_vhcl],
-        'peakvol': [peakvol]
-    })
+        peakvol = st.number_input("Peak Volume", min_value=0.0)
+
+    return pd.DataFrame(
+        {
+            "pc85th_kmh": [pc85th_kmh],
+            "volume_12h": [volume_12h],
+            "volume_24h": [volume_24h],
+            "comm_vhcl": [comm_vhcl],
+            "peakvol": [peakvol],
+        }
+    )
+
 
 def categorize_traffic_level_updated(speed_range):
-    if speed_range == 'Between 30km/h and 40km/h' or speed_range == 'Less than 30km/h':
-        return 'low'
-    elif speed_range == 'Between 40km/h and 50km/h':
-        return 'moderate'
-    elif speed_range == 'Between 50km/h and 60km/h':
-        return 'high'
-    elif speed_range == 'More than 60km/h':
-        return 'extremely high'
+    if speed_range == "Between 30km/h and 40km/h" or speed_range == "Less than 30km/h":
+        return "low"
+    elif speed_range == "Between 40km/h and 50km/h":
+        return "moderate"
+    elif speed_range == "Between 50km/h and 60km/h":
+        return "high"
+    elif speed_range == "More than 60km/h":
+        return "extremely high"
     else:
-        return 'undefined'
+        return "undefined"
 
-#User Inputs
-input_df = user_input_features()
 
-#Button to Display Result
-col1, col2, col3 = st.columns(3)  # Create three columns for the buttons
+# User Input
+input_df = user_input()
+
+# Button to Display Result
+col1, col2, col3 = st.columns(3)
 with col1:
-    if st.button('Random Forest'):
+    if st.button("Random Forest"):
         prediction = lr_model.predict(input_df)
         predicted_traffic_level = categorize_traffic_level_updated(prediction[0])
-        st.write(f'Predicted Traffic Range: {prediction[0]}')
-        st.write(f'Predicted Traffic Level: {predicted_traffic_level}')
+        st.write(f"Predicted Traffic Range: {prediction[0]}")
+        st.write(f"Predicted Traffic Level: {predicted_traffic_level}")
 
 with col2:
-    if st.button('Logistic Model'):
+    if st.button("Logistic Model"):
         prediction = logistic_model.predict(input_df)
         predicted_traffic_level = categorize_traffic_level_updated(prediction[0])
-        st.write(f'Predicted Traffic Range: {prediction[0]}')
-        st.write(f'Predicted Traffic Level: {predicted_traffic_level}')
+        st.write(f"Predicted Traffic Range: {prediction[0]}")
+        st.write(f"Predicted Traffic Level: {predicted_traffic_level}")
 
 with col3:
-    if st.button('Decision Tree'):
+    if st.button("Decision Tree"):
         prediction = decision_model.predict(input_df)
         predicted_traffic_level = categorize_traffic_level_updated(prediction[0])
-        st.write(f'Predicted Traffic Range: {prediction[0]}')
-        st.write(f'Predicted Traffic Level: {predicted_traffic_level}')
+        st.write(f"Predicted Traffic Range: {prediction[0]}")
+        st.write(f"Predicted Traffic Level: {predicted_traffic_level}")
